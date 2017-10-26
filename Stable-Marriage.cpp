bd6463125f3cbe4a69b9b9f8fc615d99b22db201
627 <rebornplusplus@gmail.com>
2017-10-26 20:03:28 +0600

    Stable Marriage

--- Stable-Marriage.cpp
+++ Stable-Marriage.cpp
@@ -5,8 +5,8 @@ using namespace std;
 // both men and women are ranked from 0....n-1
 // produces the optimal result for men ( who proposes )
 struct StableMarriage {
-	vector< vector<int> > mpref, wpref;   // Men and Women preference
-	vector<int> mpart;   // Men Partner for women, the actual result
+	vector< vector<int> > mpref, wpref;	// Men and Women preference
+	vector<int> mpart;	// Men Partner for women, the actual result
 
 	StableMarriage() { }
 	StableMarriage(int n) {
@@ -17,44 +17,44 @@ struct StableMarriage {
 	}
 
 	void stabilize(int n) {
-	    // rank[w][m] means the preference rank of man 'm' in the list of woman 'w'
-	    int rank[n][n];
-	    for(int w=0; w<n; ++w)
-	        for(int i=0; i<n; ++i)
-	            rank[w][wpref[w][i]] = i;
+		// rank[w][m] means the preference rank of man 'm' in the list of woman 'w'
+		int rank[n][n];
+		for(int w=0; w<n; ++w)
+			for(int i=0; i<n; ++i)
+				rank[w][wpref[w][i]] = i;
 
-	    // proposed[m][w] holds whether m has proposed to w or not
-	    bool proposed[n][n];
-	    memset(proposed, false, sizeof proposed);
+		// proposed[m][w] holds whether m has proposed to w or not
+		bool proposed[n][n];
+		memset(proposed, false, sizeof proposed);
 		fill(mpart.begin(), mpart.end(), -1);
 
-	    // the queue of freemen
-	    queue<int> q;
-	    for(int i=0; i<n; ++i) q.push(i);
+		// the queue of freemen
+		queue<int> q;
+		for(int i=0; i<n; ++i) q.push(i);
 
-	    while(!q.empty()) {
-	        int m = q.front();
-	        q.pop();
+		while(!q.empty()) {
+			int m = q.front();
+			q.pop();
 
-	        for(int i=0; i<n; ++i) {
-	            int w = mpref[m][i];
-	            if(proposed[m][w]) continue;    // Never propose twice
+			for(int i=0; i<n; ++i) {
+				int w = mpref[m][i];
+				if(proposed[m][w]) continue;	// Never propose twice
 
-	            proposed[m][w] = true;
-	            if(mpart[w] == -1) {
-	                mpart[w] = m;	// w is free, marry her for the time being :3
-	                break;
-	            }
-	            else {
-	                int cur = mpart[w];
+				proposed[m][w] = true;
+				if(mpart[w] == -1) {
+					mpart[w] = m;	// w is free, marry her for the time being :3
+					break;
+				}
+				else {
+					int cur = mpart[w];
 					if(rank[w][m] < rank[w][cur]) {
 						q.push(cur);	// w divorces cur
 						mpart[w] = m;	// and marries m
 						break;
 					}
-	            }
-	        }
-	    }
+				}
+			}
+		}
 	}
 };
 
@@ -62,14 +62,14 @@ int main() {
 	int n;
 	cin >> n;
 
-	StableMarriage t(n);
-	for(int i=0; i<n; ++i) for(int j=0; j<n; ++j) cin >> t.mpref[i][j];
-	for(int i=0; i<n; ++i) for(int j=0; j<n; ++j) cin >> t.wpref[i][j];
+	StableMarriage ob(n);
+	for(int i=0; i<n; ++i) for(int j=0; j<n; ++j) cin >> ob.mpref[i][j];
+	for(int i=0; i<n; ++i) for(int j=0; j<n; ++j) cin >> ob.wpref[i][j];
 
-	t.stabilize(n);
+	ob.stabilize(n);
 
 	cout << "W - M\n";
-	for(int w=0; w<n; ++w) cout << w << " - " << t.mpart[w] << "\n";
+	for(int w=0; w<n; ++w) cout << w << " - " << ob.mpart[w] << "\n";
 
 	return 0;
 }
