153f4a26ff59f5eb881899f964499fb9f465719a
rebornplusplus <rebornplusplus@gmail.com>
2018-02-27 05:36:47 +0600

    Update lca-sparse-table.cpp

--- lca-sparse-table.cpp
+++ lca-sparse-table.cpp
@@ -1,98 +1,73 @@
-// Lowest Common Ancestor
-// Using sparse table
-// refer: http://www.shafaetsplanet.com/?p=1831
-
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 100000 + 5;	// max nodes
-vector<int> g[N];		// the graph
-int par[N];			// parent of each node
-int lev[N];			// level of each node. level of root = 0
-int sp[N][18];			// sp[i][j] indicates i'th node's 2^j th parent
-bool vis[N];			// for dfs
-int n;				// actual number of nodes
+const int N = 100000 + 5;
+vector<int> g[N];
+int par[N], lev[N];
+int sp[N][18];  // sparse table, i's 2^j th parent
+int n;
 
-// traverse the graph. Get the parents and levels
 void dfs(int u, int pr, int lv) {
-	vis[u] = true;
-	par[u] = pr;
-	lev[u] = lv;
-
-	for(int i=0; i<(int) g[u].size(); ++i) {
-		int v = g[u][i];
-		if(!vis[v]) dfs(v, u, lv+1);
-	}
+    par[u] = pr;
+    lev[u] = lv;
+    for(int v : g[u]) {
+        if(v != pr) dfs(v, u, lv+1);
+    }
 }
 
-// lca initialization
 void lca_init() {
-	memset(vis, false, sizeof vis);
-	dfs(1, -1, 0);
+    dfs(1, -1, 0);
+    memset(sp, -1, sizeof sp);
+    for(int i=1; i<=n; ++i) sp[i][0] = par[i];
 
-	memset(sp, -1, sizeof sp);
-	for(int i=1; i<=n; ++i) sp[i][0] = par[i];
-
-
-	for(int j=1; (1<<j) < n; ++j) {
-		for(int i=1; i<=n; ++i) {
-			if(sp[i][j-1] != -1) {
-				sp[i][j] = sp[sp[i][j-1]][j-1];
-			}
-		}
-	}
+    for(int j=1; (1<<j) < n; ++j) {
+        for(int i=1; i<=n; ++i) {
+            if(sp[i][j-1] != -1) sp[i][j] = sp[sp[i][j-1]][j-1];
+        }
+    }
 }
 
-// returns the LCA of u and v
 int lca(int u, int v) {
-	if(lev[u] < lev[v]) swap(u, v);
+    if(lev[u] < lev[v]) swap(u, v);
 
-	int logn = 0;
-	while((1 << (1+logn)) <= n) ++logn;
+    int logn = 0;
+    while((1 << (1+logn)) <= n) ++logn;
+    for(int i=logn; i>=0; --i) {
+        if(lev[u] - (1<<i) >= lev[v]) {
+            u = sp[u][i];
+        }
+    }
 
-	for(int i=logn; i>=0; --i) {
-		if(lev[u] - (1<<i) >= lev[v]) {
-			u = sp[u][i];
-		}
-	}
-
-	if(u == v) return u;
-
-	for(int i=logn; i>=0; --i) {
-		if(sp[u][i] != -1 && sp[u][i] != sp[v][i]) {
-			u = sp[u][i];
-			v = sp[v][i];
-		}
-	}
-
-	return par[u];
+    if(u == v) return u;
+    for(int i=logn; i>=0; --i) {
+        if(sp[u][i] != -1 && sp[u][i] != sp[v][i]) {
+            u = sp[u][i];
+            v = sp[v][i];
+        }
+    }
+    return par[u];
 }
 
-// tester function
 int main() {
-	cin >> n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 
-	// clear graph
-	for(int i=1; i<=n; ++i) g[i].clear();
+    int n;
+    cin >> n;
+    for(int i=1; i<n; ++i) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
 
-	// input tree
-	for(int i=1; i<n; ++i) {
-		int u, v;
-		cin >> u >> v;
-		g[u].push_back(v);
-		g[v].push_back(u);
-	}
+    lca_init();
+    int q;
+    while(q--) {
+        int u, v;
+        cin >> u >> v;
+        cout << "lca: " << lca(u, v) << "\n";
+    }
 
-	lca_init();
-
-	// query
-	int q;
-	cin >> q;
-	while(q--) {
-		int u, v;
-		cin >> u >> v;
-		cout << lca(u, v) << "\n";
-	}
-
-	return 0;
+    return 0;
 }
