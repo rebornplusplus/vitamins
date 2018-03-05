bd8c2ac3dea0af86ee218eda0547cc45c3b85b74
rebornplusplus <rebornplusplus@gmail.com>
2018-03-06 01:00:27 +0600

    Order Statistics Tree ( OST )

--- ost.cpp
+++ ost.cpp
@@ -25,23 +25,23 @@ ordered_set;
 
 int main() {
 	ordered_set X;
-    X.insert(1);
-    X.insert(2);
-    X.insert(4);
-    X.insert(8);
-    X.insert(16);
+	X.insert(1);
+	X.insert(2);
+	X.insert(4);
+	X.insert(8);
+	X.insert(16);
 
-    cout << *X.find_by_order(1) << '\n';	// 2
-    cout << *X.find_by_order(2) << '\n';	// 4
-    cout << *X.find_by_order(4) << '\n';	// 16
-    cout << boolalpha << (X.end() == X.find_by_order(6)) << '\n';	// true
+	cout << *X.find_by_order(1) << '\n';	// 2
+	cout << *X.find_by_order(2) << '\n';	// 4
+	cout << *X.find_by_order(4) << '\n';	// 16
+	cout << boolalpha << (X.end() == X.find_by_order(6)) << '\n';	// true
 	cout << boolalpha << (X.begin() == X.find_by_order(1)) << '\n';	// false
 
-    cout << X.order_of_key(-5) << '\n';		// 0
-    cout << X.order_of_key(1) << '\n';		// 0
-    cout << X.order_of_key(3) << '\n';		// 2
-    cout << X.order_of_key(4) << '\n';		// 2
-    cout << X.order_of_key(400) << '\n';	// 5
+	cout << X.order_of_key(-5) << '\n';		// 0
+	cout << X.order_of_key(1) << '\n';		// 0
+	cout << X.order_of_key(3) << '\n';		// 2
+	cout << X.order_of_key(4) << '\n';		// 2
+	cout << X.order_of_key(400) << '\n';	// 5
 
 	return 0;
 }
